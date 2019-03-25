e65e325107c2f984a6b594004e46fbd4800e9e96
SimoneVizzuso <simonevizzuso@outlook.com>
2019-03-25 18:45:44 +0100

    booking view completed

--- src/app/dashboard/booking/booking.component.ts
+++ src/app/dashboard/booking/booking.component.ts
@@ -4,7 +4,7 @@ import { Observable, of } from 'rxjs';
 import { startWith, map } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { BookingService } from './booking.service';
-import { DatePipe } from '@angular/common';
+import { DatePipe, formatDate } from '@angular/common';
 
 import { Subject } from 'src/app/model/subject';
 import { Teacher } from 'src/app/model/teacher';
@@ -17,14 +17,29 @@ import { Teacher } from 'src/app/model/teacher';
 })
 export class BookingComponent implements OnInit {
 
+  get dateSys() {
+    var date: Date = this.lessonFormGroup.get("dateCtrl").value
+    return formatDate(date, "yyyy-MM-dd", 'en-US')
+  }
+
   get date() {
-    var date: Date = this.lessonFormGroup.get("lessonCtrl").value
-    return new DatePipe('en-US').transform(date, 'shortDate');
+    var date: Date = this.lessonFormGroup.get("dateCtrl").value
+    if (!date) return ''
+    return formatDate(date, "dd/MM/yyyy", 'en-US')
+  }
+
+
+  get slot(): string{
+    return this.lessonFormGroup.controls['slotCtrl'].value
   }
 
   subjectFormGroup: FormGroup;
   teacherFormGroup: FormGroup;
   lessonFormGroup: FormGroup;
+  slotFormGroup: FormGroup;
+
+  todayDate: Date = new Date();
+  slotOption: Boolean[] = [true, true, false]
 
   subjects: Subject[]
   subjectOption: string[] = [];
@@ -33,26 +48,22 @@ export class BookingComponent implements OnInit {
   subjectFilteredOptions: Observable<string[]>;
   teacherFilteredOptions: Observable<string[]>;
 
+  constructor(private _formBuilder: FormBuilder, private bookingService: BookingService) { }
+
   dateFilter = (date: Date): boolean => {
     const day = date.getDay();
     return day !== 0 && day !== 6;
   }
-  
-  constructor(private _formBuilder: FormBuilder, private bookingService: BookingService) { }
 
   ngOnInit() {
     this._initSubjects()
     this._initTeachers()
-    this.lessonFormGroup = this._formBuilder.group({
-      lessonCtrl: ['', Validators.required]
-    })
+    this._initLesson()
     this.bookingService.getSubjects().subscribe(
       (res: Subject[]) => {
         this.subjects = res
         this.subjectOption.splice(0, this.subjectOption.length)
-        res.forEach(x => {
-          this.subjectOption.push(x.name)
-        })
+        res.forEach( x => this.subjectOption.push(x.name) )
         this._initSubjects()
       }
     )
@@ -60,30 +71,31 @@ export class BookingComponent implements OnInit {
       (res: Teacher[]) => {
         this.teachers = res
         this.teacherOption.splice(0, this.teacherOption.length)
-        res.forEach(x => {
-          this.teacherOption.push(x.surname)
-        })
+        res.forEach( x => this.teacherOption.push(x.surname) )
         this._initTeachers()
       }
     )
   }
+
+  private _initLesson() {
+    this.lessonFormGroup = this._formBuilder.group({
+      dateCtrl: ['', Validators.required],
+      slotCtrl: ['', Validators.required]
+    })
+  }
+
   private _initSubjects() {
     this.subjectFormGroup = this._formBuilder.group({
       subjectCtrl: ['', [Validators.required, this._forbiddenNameValidator(this.subjectOption)]]
     });
-    this.teacherFormGroup = this._formBuilder.group({
-      teacherCtrl: ['', [Validators.required, this._forbiddenNameValidator(this.teacherOption)]]
-    });
-    this.lessonFormGroup = this._formBuilder.group({
-      lessonCtrl: ['', [Validators.required]],
-    })
-    //Gestione filtri
+
     this.subjectFilteredOptions = this.subjectFormGroup.controls['subjectCtrl'].valueChanges
       .pipe(
         startWith(''),
         map(value => this._filter(value, this.subjectOption))
       );
   }
+
   private _initTeachers() {
     this.teacherFormGroup = this._formBuilder.group({
       teacherCtrl: ['', [Validators.required, this._forbiddenNameValidator(this.teacherOption)]]
@@ -110,11 +122,15 @@ export class BookingComponent implements OnInit {
     let selectedIndex: number = (event.selectedIndex)
     let selectedSubject: string = this.subjectFormGroup.controls['subjectCtrl'].value
     let selectedSubjectIndex: number = this.subjects.map(s => s.name).indexOf(selectedSubject)
+    let selectedDate: string = this.lessonFormGroup.controls['dateCtrl'].value
+    let selectedSlot: string = this.lessonFormGroup.controls['slotCtrl'].value
     switch(selectedIndex) {
       case 1: 
         this.bookingService.getTeachers(selectedSubjectIndex + 1)
         this._initTeachers()
         break;
+      case 3:
+        console.log(`selectedDate = ${this.date}`, `selectedSlot = ${selectedSlot}`)
     }
   }
 }
